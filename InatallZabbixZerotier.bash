aff7169ca0e4e1348b48377b858613db3d597180
Widchayapon <101374291+widchayapon@users.noreply.github.com>
2023-10-16 22:22:51 +0700

    Update InatallZabbixZerotier.bash

--- InatallZabbixZerotier.bash
+++ InatallZabbixZerotier.bash
@@ -1,25 +1,37 @@
 #!/bin/bash
 ZABBIX_SERVER="172.20.10.4"
 
+# Get the IP address of this machine
+AGENT_IP=$(ip addr show | grep -oP 'inet \K[\d.]+')
+
+# Get the hostname of this machine
+AGENT_HOSTNAME=$(hostname)
+
 # Install Zabbix Agent
 sudo apt update
 sudo apt install zabbix-agent -y
 
-# Backup Zabbix Agent 
-sudo mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.original
-sudo cp /etc/zabbix/zabbix_agentd.conf.original /etc/zabbix/zabbix_agentd.conf
-
 # Update Zabbix Agent configuration
 sudo sed -i "s+Server=127.0.0.1+Server=$ZABBIX_SERVER+g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s+ServerActive=127.0.0.1+ServerActive=$ZABBIX_SERVER+g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s+# ListenIP=0.0.0.0+ListenIP=$AGENT_IP+g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s+# Hostname=Zabbix server+Hostname=$AGENT_HOSTNAME+g" /etc/zabbix/zabbix_agentd.conf
 
-# Enable Zabbix Agent 
+# Enable and start Zabbix Agent
 sudo systemctl enable zabbix-agent
 sudo systemctl start zabbix-agent
 
 # Install ZeroTier
-curl -s https://install.zerotier.com | sudo bash
+if ! command -v zerotier-one &>/dev/null; then
+    curl -s https://install.zerotier.com | sudo bash
+    if [ $? -eq 0 ]; then
+        echo "ZeroTier installed successfully."
+    else
+        echo "Error installing ZeroTier."
+        exit 1
+    fi
+else
+    echo "ZeroTier is already installed."
+fi
 
 echo "Zabbix Agent and ZeroTier have been installed and configured."
-
-
